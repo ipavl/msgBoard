0e96005bce2538a0ca34847f31fe35e763395d52
ipavl <ipavl@users.sourceforge.net>
2012-12-24 11:14:53 -0500

    Version number update

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@
 	/* Application settings */
 	$appSiteName = "Derp Co. Forums";
 	$appLicensedTo = "Unlicensed copy";
-	$appVersion = "v0.2-alpha";
+	$appVersion = "v0.2.3-alpha";
 	$appShowVersion = true;
 	$appShowAdminButton = true;
 	
