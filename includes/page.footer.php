288cf6052a51d48eed559b74f524ef64580478f9
ipavl <ipavl@users.sourceforge.net>
2012-12-26 21:42:17 -0500

    Moved copyright notice to bottom of thread. Next: below thread?

--- includes/page.footer.php
+++ includes/page.footer.php
@@ -1,10 +1,9 @@
-</div>
-
-<div id="copyright-notice">
-	<?php
-		echo 'Licensed to: ' . $appLicensedTo . '.<br />';
-	?>
-	Powered by <a href="https://github.com/ipavl/msgboard">msgBoard</a>. Copyright ©2012 ipavl. All rights reserved.
+	<div id="copyright-notice">
+		<?php
+			echo 'Licensed to: ' . $appLicensedTo . '.<br />';
+		?>
+		Powered by <a href="https://github.com/ipavl/msgboard">msgBoard</a>. Copyright ©2012 ipavl. All rights reserved.
+	</div>
 </div>
 
 </body>
